f01880f347f1bac53718bec98c6b5e8427aa0f78
SobczakL <lucassobczak3@gmail.com>
2024-09-08 22:10:53 -0400

    updated lsp and treesitter

--- lua/sobczakl/lazy/lsp.lua
+++ lua/sobczakl/lazy/lsp.lua
@@ -1,115 +1,112 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
+        -- Mason for managing LSP and tools
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+        "j-hui/fidget.nvim", -- LSP progress indicator
+
+        -- null-ls for Prettier formatting and linting
+        "jose-elias-alvarez/null-ls.nvim",
     },
 
     config = function()
+        -- Load required modules
+        local lspconfig = require('lspconfig')
         local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+        local null_ls = require("null-ls")
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-        require("fidget").setup({})
+        -- Setup Mason and Mason-LSPconfig
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
-                "cssls",
-                "css_variables",
-                "tailwindcss",
-                "cssmodules_ls",
-                "html",
-                "ast_grep",
-                "eslint",
-                "sqls",
+                "tsserver", -- JavaScript/TypeScript/JSX
+                "eslint", -- Linting for JavaScript/TypeScript
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
         })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        -- Setup null-ls for Prettier and ESLint
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier.with({
+                    extra_args = { "--use-tabs", "--tab-width", "4" }, -- Set Prettier to use tabs
+                }),
+                null_ls.builtins.diagnostics.eslint, -- ESLint for linting
+            },
+        })
 
+        -- LSP handlers setup (for specific LSPs like tsserver, eslint)
+        require("mason-lspconfig").setup_handlers({
+            function(server_name) -- Default handler
+                lspconfig[server_name].setup({
+                    capabilities = capabilities
+                })
+            end,
+            -- Specific configuration for tsserver (JavaScript/TypeScript)
+            ["tsserver"] = function()
+                lspconfig.tsserver.setup({
+                    capabilities = capabilities,
+                    on_attach = function(client, bufnr)
+                        -- Disable tsserver formatting (we'll use Prettier instead)
+                        client.server_capabilities.documentFormattingProvider = false
+                    end,
+                    root_dir = lspconfig.util.root_pattern("package.json"),
+                })
+            end,
+            -- Specific configuration for eslint (JavaScript/TypeScript linting)
+            ["eslint"] = function()
+                lspconfig.eslint.setup({
+                    capabilities = capabilities,
+                    root_dir = lspconfig.util.root_pattern(".eslintrc.js", ".eslintrc.json", "package.json"),
+                })
+            end,
+        })
+
+        -- Setup nvim-cmp for autocompletion
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require('luasnip').lsp_expand(args.body) -- For LuaSnip users.
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<tab>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<C-k>'] = cmp.mapping.select_prev_item(),
+                ['<C-j>'] = cmp.mapping.select_next_item(),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
             }),
-            sources = cmp.config.sources({
+            sources = {
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
+                { name = 'luasnip' },
                 { name = 'buffer' },
-            })
+                { name = 'path' },
+            }
         })
-
+        -- Diagnostics configuration
         vim.diagnostic.config({
-            -- update_in_insert = true,
             float = {
                 focusable = false,
                 style = "minimal",
                 border = "rounded",
                 source = "always",
-                header = "",
-                prefix = "",
             },
         })
+
+        -- Optionally, setup fidget for LSP progress
+        require("fidget").setup({})
     end
 }
--- lua/sobczakl/lazy/treesitter.lua
+++ lua/sobczakl/lazy/treesitter.lua
@@ -1,7 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    dependencies = {
+        -- Autocomplete HTML and JSX tags
+        "windwp/nvim-ts-autotag",
+    },
     config = function()
+        -- Setup Treesitter
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
@@ -13,25 +18,21 @@ return {
             sync_install = false,
 
             -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
             auto_install = true,
 
+            -- Enable indentation
             indent = {
                 enable = true
             },
 
+            -- Enable highlighting
             highlight = {
-                -- `false` will disable the whole extension
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { "markdown" },
             },
         })
 
+        -- Set up custom parser for templ
         local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
         treesitter_parser_config.templ = {
             install_info = {
@@ -42,5 +43,8 @@ return {
         }
 
         vim.treesitter.language.register("templ", "templ")
+
+        -- Setup nvim-ts-autotag separately
+        require('nvim-ts-autotag').setup()
     end
 }
